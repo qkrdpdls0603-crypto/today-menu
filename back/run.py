7bf11b8d95575114d1164ea07c43a703e7981885
sdhuen <sdhuen01@gmail.com>
2026-07-02 12:05:16 +0900

    Update run.py

--- back/run.py
+++ back/run.py
@@ -2,6 +2,14 @@ import os
 import eventlet
 eventlet.monkey_patch()
 
+# DATABASE_URL 강제 설정 확인
+db_url = os.environ.get('DATABASE_URL', '')
+if db_url:
+    os.environ['SQLALCHEMY_DATABASE_URI'] = db_url
+    print(f"DB URL: {db_url[:40]}")
+else:
+    print("WARNING: DATABASE_URL not set!")
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,5 +19,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('FLASK_ENV') == 'development'
-    socketio.run(app, host='0.0.0.0', port=port, debug=debug)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
